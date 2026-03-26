2e1481f357bad7dfde0eecdebcf228a34a9a6dcd
Luis Osvaldo Torres <luis.torrest@elektra.com.mx>
2026-03-26 11:50:57 -0600

    Documentation Annotations.kt

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -5,19 +5,10 @@ plugins {
 }
 publishing {
     publications {
-        // Creamos una publicación llamada "maven"
         create<MavenPublication>("maven") {
-            // El Group ID suele ser la ruta de tu repositorio en GitHub
-            // Ejemplo: com.github.tuUsuarioDeGithub
             groupId = "com.github.nautiklot"
-
-            // El nombre específico de este módulo
             artifactId = "annotations"
-
-            // La versión inicial de tu librería
             version = "1.0.1"
-
-            // Le indicamos que empaquete el código compilado usando el componente de Java/Kotlin
             from(components["java"])
         }
     }
